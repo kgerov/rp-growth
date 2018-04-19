e9ac49ad64b98faac66d533e0c3b96940287ff9c
kgerov <kgerov@abv.bg>
2018-04-19 15:58:29 +0300

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='rp-growth',
+    version='1.0',
+    description='Python implementation of the RP-Growth algorithm',
+    author='Konstantin Gerov',
+    author_email='kosyo.gerov@gmail.com',
+    url='https://github.com/kgerov/rp-growth',
+)
